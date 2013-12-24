219fc562ab9f81ff94d1fe3c88b3a4e83b940a3c
Stephen Mathieson <smath23@gmail.com>
2013-12-24 15:30:57 -0500

    Add support for operations on a path other than .
    I'm following the same sphia(1)-style path handling (I think):
    
      - -p|--path
      - SPHIA_PATH
      - pwd

--- src/main.c
+++ src/main.c
@@ -1,14 +1,32 @@
 
+//
+// main.c
+//
+// Copyright (c) 2013 Stephen Mathieson
+// MIT licensed
+//
+
 #include <stdio.h>
 #include <sophia/sophia.h>
 #include <sophia/sp.h>
+#include "commander/commander.h"
 
-int main(int argc, char *argv[]) {
-  void *env = sp_env();
+static void *env;
+static void *db;
+static char path[256];
+
+static void
+set_path(command_t *self) {
+  strcpy(path, self->arg);
+}
+
+static int
+batchput_init() {
+  env = sp_env();
   int rc;
   if (NULL == env) return 1;
 
-  rc = sp_ctl(env, SPDIR, SPO_CREAT|SPO_RDWR, ".");
+  rc = sp_ctl(env, SPDIR, SPO_CREAT|SPO_RDWR, path);
   if (-1 == rc) {
     fprintf(stderr, "Error creating or reading database\n");
     sp_destroy(env);
@@ -22,32 +40,67 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  void *db = sp_open(env);
+  db = sp_open(env);
   if (NULL == db) {
     fprintf(stderr, "%s\n", sp_error(env));
     sp_destroy(env);
     return 1;
   }
 
-  for (int i = 1; i < argc; i++) {
-    if (0 == i % 2) {
-      char *key = strdup(argv[i - 1]);
-      char *value = strdup(argv[i]);
-      rc = sp_set(db, key, strlen(key), value, strlen(value));
-      if (-1 == rc) {
-        fprintf(stderr
-          , "An error occured setting key '%s'. %s"
-          , key
-          , sp_error(db));
-        free(key);
-        free(value);
-        sp_destroy(db);
-        sp_destroy(env);
-        return 2;
-      }
+  return 0;
+}
+
+int
+main(int argc, char *argv[]) {
+  command_t program;
+  command_init(&program
+    , "sphia-batchput"
+    , "0.0.1");
+  command_option(&program
+    , "-p"
+    , "--path <path>"
+    , "path to database"
+    , set_path);
+  command_parse(&program, argc, argv);
+
+  if (0 != program.argc % 2) {
+    fprintf(stderr, "Invalid number of key/value paris\n");
+    return 1;
+  }
+
+  if (0 == strlen(path)) {
+    char *default_path = getenv("SPHIA_PATH");
+    if (NULL != default_path) {
+      // support for `$ SPHIA_PATH=foo sphia batchput foo bar`
+      strcpy(path, default_path);
+    } else {
+      // default to pwd
+      strcpy(path, ".");
+    }
+  }
+
+  int rc;
+  if (0 != (rc = batchput_init())) return rc;
+
+  for (int i = 0; i < program.argc; i += 2) {
+    char *key = strdup(program.argv[i]);
+    char *value = strdup(program.argv[i + 1]);
+
+    rc = sp_set(db, key, strlen(key), value, strlen(value));
+    if (-1 == rc) {
+      fprintf(stderr
+        , "An error occured setting key '%s'. %s"
+        , key
+        , sp_error(db));
       free(key);
       free(value);
+      sp_destroy(db);
+      sp_destroy(env);
+      return 1;
     }
+
+    free(key);
+    free(value);
   }
 
   sp_destroy(db);
